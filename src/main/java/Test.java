410df1be0b1e43d074e6e955e8153ae79067c2b4
huangliangc <huangliangc@enn.cn>
2018-04-25 09:14:31 +0800

    add test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Test{
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         System.out.println("abc");
         System.out.println("def");
         System.out.println("ghi");
         System.out.println("jkl");
+
+        List<String> list = Arrays.asList("a","b","c","d");
+        for(String s : list){
+            System.out.println(s);
+        }
+        list.stream().forEach(x -> System.out.println(x));//java8 lambda
+
+
     }
 }
